96fe84e0bb12ff606e3683f2b4df066be5f8a53a
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-02-05 08:09:07 +0530

    allowed building

--- pages/api/anotherAFINN.js
+++
@@ -1,4 +0,0 @@
-var Sentiment = require("sentiment");
-var sentiment = new Sentiment();
-var result = sentiment.analyze("Adani");
-console.dir(result); // Score: -2, Comparative: -0.666
--- pages/api/finalSentiment.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-
-axios
-    .post("http://localhost:3000/api/emotion", {
-        text: "This is a good news",
-    })
-    .then((response) => {
-        console.log(response.data);
-    });
